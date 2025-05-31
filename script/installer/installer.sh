89552f863750d73c44b76285ba5e4c47fb640f98
BullnBear789 <bullnpeak@gmail.com>
2025-05-31 23:16:12 +0700

    Change

--- script/installer/installer.sh
+++ script/installer/installer.sh
@@ -353,7 +353,7 @@ else
 fi
 
 mkdir -p /root/cocktailpi
-wget -q --show-progress https://github.com/alex9849/CocktailPi/releases/latest/download/server.jar -O /root/cocktailpi/cocktailpi.jar
+wget -q --show-progress https://github.com/BullnBear789/CocktailPi/blob/master/server.jar -O /root/cocktailpi/cocktailpi.jar
 if [ -f /etc/init.d/cocktailpi ]; then
     unlink /etc/init.d/cocktailpi
 fi
