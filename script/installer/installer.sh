add953d3065233bbd0ab19a1aaa40d10e947e457
BullnBear789 <bullnpeak@gmail.com>
2025-07-14 02:36:02 +0700

    test

--- script/installer/installer.sh
+++ script/installer/installer.sh
@@ -208,35 +208,38 @@ function restore_database {
 		cp -r -b /home/pi/Backup_CocktailPi/Backup_cocktailPi-data.db /root/cocktailpi/cocktailpi-data.db
 		rm -rf /home/pi/Backup_CocktailPi
 		echo "Successful Data Recovery"
-		sleep 2
 		echo ""
+		sleep 2
 	else
 		echo "'/home/pi/Backup_CocktailPi/Backup_cocktailPi-data.db: No such file or directory'"
-		sleep 2
+		echo ""
 		echo "Please wait..."
+		echo ""
+		sleep 2
 		if [ -f /home/pi/Backup_CocktailPi/*.db ]; then
 			cp -r -b /home/pi/Backup_CocktailPi/*.db /root/cocktailpi/cocktailpi-data.db
 			rm -rf /home/pi/Backup_CocktailPi
 			echo "Successful Data Recovery 2"
-			sleep 2
 			echo ""
+			sleep 2
 		else
 			if [ -f /home/pi/Backup_cocktailPi-data.db ]; then
 				cp -r -b /home/pi/Backup_cocktailPi-data.db /root/cocktailpi/cocktailpi-data.db
 				rm -rf /home/pi/Backup_cocktailPi-data.db
 				echo "Successful Data Recovery 3"
-				sleep 2
 				echo ""
+				sleep 2
 			else
 				if [ -f /home/pi/*.db ]; then
 					cp -r -b /home/pi/*.db /root/cocktailpi/cocktailpi-data.db
 					rm -rf /home/pi/*.db
 					echo "Successful Data Recovery 4"
-					sleep 2
 					echo ""
-				else
-					echo "#'/home/pi/Backup_cocktailPi-data.db: No such file or directory'#"
 					sleep 2
+				else
+					echo "'/home/pi/Backup_cocktailPi-data.db: No such file or directory'"
+					echo ""
+					sleep 3
 				fi
 			fi
 		fi
